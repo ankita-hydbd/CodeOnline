43bd69019176b9f3f04d71f9e9cf36b6b23254fb
Ankita Agrawal <ankita.hydbd@gmail.com>
2023-08-29 19:51:36 +0530

    Added spring boot dependency to create web application.

---
+++ src/main/java/com/ankita/codeonline/CodeOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.ankita.codeonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeOnlineApplication.class, args);
+	}
+
+}
